b97a0b7ad47c3c9e7e7b2b7fea20be3b1498dd3b
uczen <a@a>
2024-04-03 13:28:32 +0200

    a

--- src/main/java/com/example/kolizja/HelloApplication.java
+++ src/main/java/com/example/kolizja/HelloApplication.java
@@ -17,8 +17,8 @@ public class HelloApplication extends Application {
 
     private static final int SCENE_WIDTH = 800;
     private static final int SCENE_HEIGHT = 600;
-    private static final int BALL_RADIUS = 30;
-    private static final double BALL_SPEED = 0.6;
+    private static final int BALL_RADIUS = 10;
+    private static final double BALL_SPEED = 0.2;
 
     private double centerX;
     private double centerY;
@@ -32,18 +32,18 @@ public class HelloApplication extends Application {
 
         Group root = new Group();
         Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
+        Circle ball = new Circle();
+        Random random = new Random();
 
         stage.setTitle("Odbijanie kulki!");
         stage.setScene(scene);
 
-        centerX = Math.random() * (SCENE_WIDTH - 2 * BALL_RADIUS) + BALL_RADIUS;
-        centerY = Math.random() * (SCENE_HEIGHT - 2 * BALL_RADIUS) + BALL_RADIUS;
+        centerX = random.nextDouble() * (SCENE_WIDTH - 2 * BALL_RADIUS) + BALL_RADIUS;
+        centerY = random.nextDouble() * (SCENE_HEIGHT - 2 * BALL_RADIUS) + BALL_RADIUS;
 
-        Random random = new Random();
         angle = random.nextDouble() * 2 * Math.PI;
         deltaX = BALL_SPEED * Math.cos(angle);
         deltaY = BALL_SPEED * Math.sin(angle);
-        Circle ball = new Circle();
         ball.setRadius(BALL_RADIUS);
         ball.setCenterX(centerX);
         ball.setCenterY(centerY);
@@ -59,11 +59,9 @@ public class HelloApplication extends Application {
 
                 if (centerX + BALL_RADIUS >= SCENE_WIDTH || centerX - BALL_RADIUS <= 0){
                     deltaX *= -1;
-                    ball.setCenterX(SCENE_WIDTH-BALL_RADIUS);
                 }
                 if (centerY + BALL_RADIUS >= SCENE_HEIGHT || centerY - BALL_RADIUS <= 0){
                     deltaY *= -1;
-                    ball.setCenterX(SCENE_WIDTH-BALL_RADIUS);
                 }
 
                 ball.setCenterX(centerX);
